fa5f1280f6c27369feee072e7629818d11964591
lucaromano2712 <luca.romano2712@gmail.com>
2026-05-07 10:22:01 +0000

    Deploying to gh-pages from @ lucaromano2712/lucaromano2712.github.io@6be7f1c2f12edf023b4f0acf82c1a8ef7797f797 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -511,7 +511,7 @@ ninja.data = [{
         title: 'CV',
         section: 'Socials',
         handler: () => {
-          window.open("/assets/pdf/example_pdf.pdf", "_blank");
+          window.open("/assets/pdf/CV%20-%20Luca%20Romano%20(eng).pdf", "_blank");
         },
       },{
         id: 'social-email',
