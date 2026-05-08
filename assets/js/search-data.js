5f9b893b3c8a616d9a7d11c45f5dbd4cd1120b72
lucaromano2712 <luca.romano2712@gmail.com>
2026-05-08 09:14:02 +0000

    Deploying to gh-pages from @ lucaromano2712/lucaromano2712.github.io@4391feaad8cef9aceea1b3ee392df5278da9347d 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -416,9 +416,9 @@ ninja.data = [{
             },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
           title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
           description: "",
-          section: "News",},{id: "projects-expected-goals-xg-analysis-in-professional-soccer",
-          title: 'Expected Goals (xG) Analysis in Professional Soccer',
-          description: "Master&#39;s thesis and research paper submitted to IJSSC",
+          section: "News",},{id: "projects-project-1",
+          title: 'project 1',
+          description: "with background image",
           section: "Projects",handler: () => {
               window.location.href = "/projects/1_project/";
             },},{id: "projects-project-2",
