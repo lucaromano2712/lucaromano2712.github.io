98792bd81de595742145b03df2fee8008c028dad
lucaromano2712 <luca.romano2712@gmail.com>
2026-05-08 09:13:39 +0000

    Deploying to gh-pages from @ lucaromano2712/lucaromano2712.github.io@2112dcfacf27239e0bb99a307f229d25592acccc 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -416,9 +416,9 @@ ninja.data = [{
             },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
           title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
           description: "",
-          section: "News",},{id: "projects-project-1",
-          title: 'project 1',
-          description: "with background image",
+          section: "News",},{id: "projects-expected-goals-xg-analysis-in-professional-soccer",
+          title: 'Expected Goals (xG) Analysis in Professional Soccer',
+          description: "Master&#39;s thesis and research paper submitted to IJSSC",
           section: "Projects",handler: () => {
               window.location.href = "/projects/1_project/";
             },},{id: "projects-project-2",
@@ -431,11 +431,6 @@ ninja.data = [{
           description: "a project that redirects to another website",
           section: "Projects",handler: () => {
               window.location.href = "/projects/3_project/";
-            },},{id: "projects-project-9",
-          title: 'project 9',
-          description: "another project with an image 🎉",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/9_project/";
             },},{id: "teachings-data-science-fundamentals",
           title: 'Data Science Fundamentals',
           description: "This course covers the foundational aspects of data science, including data collection, cleaning, analysis, and visualization. Students will learn practical skills for working with real-world datasets.",
